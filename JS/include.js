b3ed07405df365b44fad167912f8295734a8864f
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2026-09-25 14:17:45 -0500

    Add files via upload

--- JS/include.js
+++ JS/include.js
@@ -1,14 +1,34 @@
 async function loadHTML(id, file) { 
     const response = await fetch(file); 
+    if (!response.ok) { throw new Error("No se pudo cargar: " + file); }
     const data = await response.text(); 
     document.getElementById(id).innerHTML = data; 
 } 
 
+async function cargarAnimacion() {
+    const imagen = new Image();
+    imagen.src = "/Portafolio/IMG/Carga.png";
+
+    imagen.onload = function () {
+        document.getElementById("loading-animation").innerHTML = '<img src="/Portafolio/IMG/Carga.png" alt="Cargando..." class="loading-image">';
+    };
+
+    imagen.onerror = function () {
+        document.getElementById("loading-animation").innerHTML = '<div class="loader"></div>';
+    };
+}
 
 async function cargarPagina() {
     document.body.style.overflow = "hidden";
 
     await loadHTML("loading", "/Portafolio/Estructura/carga.html");
+
+    await cargarAnimacion();
+
+    await Promise.all([
+        loadHTML("nav", "/Portafolio/Estructura/nav.html"),
+        loadHTML("footer", "/Portafolio/Estructura/footer.html")
+    ]);
 
     const imagenes = document.images;
 
